4bf107735d73bfd129d3d608f9b8c5c7d2aa45d9
mimosa <563576965@qq.com>
2021-09-23 17:21:46 +0800

    add SmartGuys

---
+++ SmartGuys/SmartGuys/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartGuys
+//
+//  Created by Mimosa on 2021/9/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+
+}
+
